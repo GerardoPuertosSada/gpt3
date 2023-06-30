5256416eb0e851a7a138996ee5dd11320ae1c9a7
GerardoPuertosSada <gerardopsada@gmail.com>
2023-06-30 15:05:20 -0600

    more structuring of the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import { Brand, CTA, Navbar } from './components'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
 
 
 const App = () => {
   return (
-    <div>
-      <h1>
-        GPT-3
-      </h1>
+    <div className='App'>
+      div
     </div>
   )
 }
