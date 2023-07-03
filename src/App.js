047df2811925a0242a9095e92f4534785666dd88
GerardoPuertosSada <gerardopsada@gmail.com>
2023-07-03 15:55:38 -0600

    assets folder created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Brand, CTA, Navbar } from './components'
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
-
+import './App.css'
 
 const App = () => {
   return (
     <div className='App'>
-      div
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+        </div>
+        <div className='white__bg'>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
     </div>
   )
 }
